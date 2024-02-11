a409d420cf82994d62db69dfb286863f9aba64fa
night-slayer18 <83979018+night-slayer18@users.noreply.github.com>
2024-02-11 19:26:27 +0530

    backend done

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,28 @@
 const express = require('express');
+const multer = require('multer');
 const cors = require('cors');
 
 const app = express();
-const port = 5000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true })); // Use express.urlencoded for form data
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null,'C:/Users/acer/mainproject/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.fields([{ name: 'video', maxCount: 1 }, { name: 'subtitle', maxCount: 1 }]), (req, res) => {
+  res.send('Files uploaded successfully');
+});
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
 });
