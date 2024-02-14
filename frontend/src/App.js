06c6c9ab8fc4b64d5145251eb0e6d1315ee4ff3e
night-slayer18 <83979018+night-slayer18@users.noreply.github.com>
2024-02-14 20:46:29 +0530

    added navbar and upload component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,107 +1,26 @@
-import React, { useState, useRef } from 'react';
-
+import Navbar from './components/Navbar';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+import toast, { Toaster } from 'react-hot-toast';
+import Upload from './components/Upload';
 const App = () => {
-  const [videoFile, setVideoFile] = useState(null);
-  const [subtitleFile, setSubtitleFile] = useState(null);
-  const [uploading, setUploading] = useState(false);
-  const [uploadError, setUploadError] = useState(null);
-
-  const videoRef = useRef(null);
-  const subtitleRef = useRef(null);
-
-  const handleVideoChange = (event) => {
-    setVideoFile(event.target.files[0]);
-  };
-
-  const handleSubtitleChange = (event) => {
-    setSubtitleFile(event.target.files[0]);
-  };
-
-  const handleClearFiles = () => {
-    setVideoFile(null);
-    setSubtitleFile(null);
-    videoRef.current.value = '';
-    subtitleRef.current.value = '';
-  };
-
-  const handleUploadFiles = async () => {
-    try {
-      setUploading(true);
-      setUploadError(null);
-
-      const formData = new FormData();
-      formData.append('video', videoFile);
-      formData.append('subtitle', subtitleFile);
-
-      const response = await fetch('http://localhost:5000/upload', {
-        method: 'POST',
-        body: formData,
-      });
-
-      if (response.ok) {
-        console.log('Files uploaded successfully');
-        setVideoFile(null);
-        setSubtitleFile(null);
-        videoRef.current.value = '';
-        subtitleRef.current.value = '';
-      } else {
-        console.error('File upload failed');
-        setUploadError('File upload failed. Please try again.');
-      }
-    } catch (error) {
-      console.error('Error uploading files:', error);
-      setUploadError('An error occurred. Please try again.');
-    } finally {
-      setUploading(false);
-    }
-  };
+  const uploadSuccess = () => toast.success('Files uploaded successfully');
+  const uploadError = () => toast.error('File upload failed. Please try again.');
+  const errorOccurred = () => toast.error('An error occurred. Please try again.');
 
   return (
-    <div className="container d-flex flex-column align-items-center justify-content-center mt-5">
-      <h1 className="mb-4 mt-5 ">Video Upload App</h1>
-
-      <div className="input-group mb-3">
-        <div className="container my-4">
-          <input
-            ref={videoRef}
-            type="file"
-            accept="video/*"
-            onChange={handleVideoChange}
-            className="form-control my-5 w-50 mx-auto"
-            id="inputGroupFile01"
-          />
-          <input
-            ref={subtitleRef}
-            type="file"
-            accept=".srt"
-            onChange={handleSubtitleChange}
-            className="form-control my-5 w-50 mx-auto"
-            id="inputGroupFile02"
-          />
-        </div>
-      </div>
-
-      <div className="container d-flex justify-content-center align-items-center gap-5">
-        <button
-          type="button"
-          className="btn btn-primary"
-          onClick={handleClearFiles}
-          disabled={!videoFile && !subtitleFile}
-        >
-          Clear Files
-        </button>
-        <button
-          type="button"
-          className="btn btn-primary"
-          onClick={handleUploadFiles}
-          disabled={!videoFile || !subtitleFile || uploading}
-        >
-          {uploading ? 'Uploading...' : 'Upload Files'}
-        </button>
-      </div>
-
-      {uploadError && <div className="text-danger mt-3">{uploadError}</div>}
-    </div>
+    <>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/upload" element={<Upload/>} uploadSuccess={uploadSuccess} uploadError={uploadError} errorOccurred={errorOccurred} />
+      </Routes>
+      <Toaster/>
+    </BrowserRouter>
+    </>
   );
 };
 
---
+++ frontend/src/components/Upload.js
@@ -0,0 +1,113 @@
+import React, { useState, useRef } from 'react';
+
+
+const Upload = (props) => {
+  const [videoFile, setVideoFile] = useState(null);
+  const [subtitleFile, setSubtitleFile] = useState(null);
+  const [uploading, setUploading] = useState(false);
+  const [uploadError, setUploadError] = useState(null);
+
+  const videoRef = useRef(null);
+  const subtitleRef = useRef(null);
+
+  const handleVideoChange = (event) => {
+    setVideoFile(event.target.files[0]);
+  };
+
+  const handleSubtitleChange = (event) => {
+    setSubtitleFile(event.target.files[0]);
+  };
+
+  const handleClearFiles = () => {
+    setVideoFile(null);
+    setSubtitleFile(null);
+    videoRef.current.value = '';
+    subtitleRef.current.value = '';
+  };
+
+  const handleUploadFiles = async () => {
+    try {
+      setUploading(true);
+      setUploadError(null);
+
+      const formData = new FormData();
+      formData.append('video', videoFile);
+      formData.append('subtitle', subtitleFile);
+
+      const response = await fetch('http://localhost:5000/upload', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (response.ok) {
+        console.log('Files uploaded successfully');
+        setVideoFile(null);
+        setSubtitleFile(null);
+        videoRef.current.value = '';
+        subtitleRef.current.value = '';
+        props.uploadSuccess();
+      } else {
+        console.error('File upload failed');
+        setUploadError('File upload failed. Please try again.');
+        props.uploadError();
+      }
+    } catch (error) {
+      console.error('Error uploading files:', error);
+      setUploadError('An error occurred. Please try again.');
+        props.errorOccurred();
+    } finally {
+      setUploading(false);
+    }
+  };
+  return (
+    <div>
+      <div className="container d-flex flex-column align-items-center justify-content-center mt-5">
+      <h1 className="mb-4 mt-5 ">Video Upload App</h1>
+
+      <div className="input-group mb-3">
+        <div className="container my-4">
+          <input
+            ref={videoRef}
+            type="file"
+            accept="video/*"
+            onChange={handleVideoChange}
+            className="form-control my-5 w-50 mx-auto"
+            id="inputGroupFile01"
+          />
+          <input
+            ref={subtitleRef}
+            type="file"
+            accept=".srt"
+            onChange={handleSubtitleChange}
+            className="form-control my-5 w-50 mx-auto"
+            id="inputGroupFile02"
+          />
+        </div>
+      </div>
+
+      <div className="container d-flex justify-content-center align-items-center gap-5">
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={handleClearFiles}
+          disabled={!videoFile && !subtitleFile}
+        >
+          Clear Files
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={handleUploadFiles}
+          disabled={!videoFile || !subtitleFile || uploading}
+        >
+          {uploading ? 'Uploading...' : 'Upload Files'}
+        </button>
+      </div>
+
+      {uploadError && <div className="text-danger mt-3">{uploadError}</div>}
+    </div>
+    </div>
+  )
+}
+
+export default Upload
